f6ea8ded144313d541e00f78c70705d1286a5738
praneet <praneet18.ark@gmail.com>
2025-04-18 20:26:58 +0530

    UI/backend changes

--- backend/Algorithm.py
+++ backend/Algorithm.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, send_file
 from PIL import Image
 import io
+import os
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
@@ -20,4 +22,5 @@ def upload():
     return send_file(output, as_attachment=True, download_name="compressed.jpg", mimetype="image/jpeg")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+
