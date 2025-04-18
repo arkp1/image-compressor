3940f2007a73f2542599db408e39076922cc9595
praneet <praneet18.ark@gmail.com>
2025-04-18 20:41:18 +0530

    backend fix,

--- backend/Algorithm.py
+++ backend/Algorithm.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/upload": {"origins": "https://compressit-puce.vercel.app"}})
+CORS(app)
 
 
 @app.route('/upload', methods=['POST'])
